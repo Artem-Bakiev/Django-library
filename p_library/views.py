7f00fba5bc32427d3e075040772ed0d634e095c7
Artem Bakiev <artembakiev@gmail.com>
2021-03-22 12:36:13 +0800

    create new view

--- p_library/views.py
+++ p_library/views.py
@@ -1,8 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 from p_library.models import Book
 
 # Create your views here.
 def books_list(request):
     books = Book.objects.all()
     return HttpResponse(books)
+
+def index(request):
+    template = loader.get_template('index.html')
+    books_count = Book.objects.all().count()
+    biblio_data = {
+        "title": "мою библиотеку",
+        "books_count": books_count,
+    }
+    return HttpResponse(template.render(biblio_data))
+    
