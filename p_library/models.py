618b87d92c44f5954be32dd78b43e31bda1ec737
Artem Bakiev <artembakiev@gmail.com>
2021-03-22 11:40:37 +0800

    Create Author Model

--- p_library/models.py
+++ p_library/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Author(models.Model):
+    full_name = models.TextField()
+    birth_year = models.SmallIntegerField()
+    country = models.CharField(max_length=2)
