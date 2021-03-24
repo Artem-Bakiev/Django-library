d750e7e1b8ba372a6cfb586072bb413188a744ff
Artem Bakiev <artembakiev@gmail.com>
2021-03-24 19:15:02 +0800

    add Author's to admin and better visualisation

--- p_library/admin.py
+++ p_library/admin.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
-from p_library.models import Book
+from p_library.models import Book, Author
 
 # Register your models here.
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author')
+
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
     pass
